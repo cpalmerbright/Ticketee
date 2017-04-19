b6c7a86e14030abc999a5db1c7b86b31201b13f6
Caitlin <c_palmerbright@hotmail.com>
2017-04-19 21:10:10 +1000

    Added protection against self-archiving for admin users

--- spec/features/admin/archiving_users_spec.rb
+++ spec/features/admin/archiving_users_spec.rb
@@ -15,4 +15,11 @@ RSpec.feature "An admin can archive users" do
     expect(page).to have_content "User has been archived"
     expect(page).not_to have_content user.email
   end
+  scenario "but cannot archive themsleves" do
+    visit admin_user_path(admin_user)
+    click_link "Archive User"
+
+    expect(page).to have_content "You cannot archive yourself!"
+    expect(page).to have_content admin_user.email
+  end
 end
